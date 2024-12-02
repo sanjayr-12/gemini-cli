4c59b49ee48616aa5358532edb1d9619b82f0f03
sanjayr-12 <sanjayr.bit22@gmail.com>
2024-12-02 18:30:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const readline = require("node:readline");
+const axios = require("axios");
+const chalk = require("chalk");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function genContent() {
+  rl.question("Enter the prompt: ", async (prompt) => {
+    try {
+      const result = await axios.post(
+        "https://gemini-35e7.onrender.com/api/prompt",
+        {
+          prompt,
+        }
+      );
+      console.log(chalk.blue.italic(result.data));
+    } catch (error) {
+      console.log(chalk.red("Server Error"));
+    }
+    genContent();
+  });
+}
+
+genContent();
